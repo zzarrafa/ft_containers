6b1692ff663a13a6a9cece6296e5fcee39a884a9
zzarrafa <zineb.zarafa@gmail.com>
2021-11-24 22:36:01 +0100

    check

--- iterators.hpp
+++ iterators.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   iterators.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zzarrafa <zzarrafa@student.42.fr>          +#+  +:+       +#+        */
+/*   By: zineb <zineb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 11:49:48 by zzarrafa          #+#    #+#             */
-/*   Updated: 2021/11/24 12:04:52 by zzarrafa         ###   ########.fr       */
+/*   Updated: 2021/11/24 22:33:31 by zineb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,43 @@
 #include <memory>
 #include <iterator>
 
-
-namespace ft{
-    
+namespace ft
+{
+    template <class Iter>
+    class iterator_traits
+    {
+        typedef
+    }
 }
-namespace ft {
+namespace ft
+{
     template <class Iterator>
-     
+
     class reverse_iterator
     {
-        private:
+    private:
         typedef Iterator iterator_type;
-        typedef iterator_traits<Iterator>::iterator_category 
-
-        
+        typedef iterator_traits<Iterator>::iterator_category
     }
-    
-   
-   
-    
+
 }
+template <typename T>
+class random_access_iterator : ft::iterator<ft::random_access_iterator_tag, T>
+{
+public:
+    /* Category of the iterator. */
+    typedef typename ft::iterator<ft::random_access_iterator_tag, T>::iterator_category iterator_category;
+
+    /* Type of elements pointed. */
+    typedef typename ft::iterator<ft::random_access_iterator_tag, T>::value_type value_type;
+
+    /* Type to represent the difference between two iterators. */
+    typedef typename ft::iterator<ft::random_access_iterator_tag, T>::difference_type difference_type;
+
+    /* Type to represent a pointer to an element pointed */
+    typedef T *pointer;
+
+    /* Type to represent a reference to an element pointed */
+    typedef T &reference;
+
+    /*
--- vector.hpp
+++ vector.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   vector.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zzarrafa <zzarrafa@student.42.fr>          +#+  +:+       +#+        */
+/*   By: zineb <zineb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/08 00:13:21 by zineb             #+#    #+#             */
-/*   Updated: 2021/11/24 12:04:16 by zzarrafa         ###   ########.fr       */
+/*   Updated: 2021/11/24 22:29:26 by zineb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,47 +16,44 @@
 #include <memory>
 #include <iterator>
 
-namespace ft{
-
-template < class T, class Alloc = std::allocator<T> >
- 
-class vector
+namespace ft
 {
-private:
-    typedef T value_type;
-    
-    typedef Alloc allocator_type;
-    
-    typedef allocator_type::reference reference ;
-    
-    typedef allocator_type::const_reference const_reference	;
 
-    typedef typenameallocator_type::pointer pointer;
-    
-   typedef	allocator_type::const_pointer const_pointer;
-   
-//    typedef typename std::random_access_iterator_tag iterator;
+    template <class T, class Alloc = std::allocator<T>>
 
-//    typedef std::random_access_iterator_tag  const_iterator;
-   
-public: 
-    vector(/* args */);
-    ~vector();
-    
-    vector(const vector &cp);
+    class vector
+    {
+    private:
+        typedef T value_type;
 
-    vector & operator=(const vector &rhs);
-    
-    void get_allocator();
-    
-};
+        typedef Alloc allocator_type;
 
-vector::vector(/* args */)
-{
-    
-}
+        typedef allocator_type::reference reference;
 
-vector::~vector()
-{
-}
+        typedef allocator_type::const_reference const_reference;
+
+        typedef typenameallocator_type::pointer pointer;
+
+        typedef allocator_type::const_pointer const_pointer;
+
+        typedef ft::random_access_iterator<value_type> iterator
+            //    typedef std::random_access_iterator_tag  const_iterator;
+
+            public : vector(/* args */);
+        ~vector();
+
+        vector(const vector &cp);
+
+        vector &operator=(const vector &rhs);
+
+        void get_allocator();
+    };
+
+    vector::vector(/* args */)
+    {
+    }
+
+    vector::~vector()
+    {
+    }
 }
