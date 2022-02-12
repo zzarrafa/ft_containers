a0c0567d3e8da29d0fb1b82c3bd5a0eaa5e28ec4
zzarrafa <zineb.zarafa@gmail.com>
2022-02-12 12:50:48 +0100

    ok

--- map/equal.hpp
+++ map/equal.hpp
@@ -34,3 +34,57 @@ namespace ft
 			return (first2!=last2);
 		}
 }
+	
+				// insertion avl
+				pair<Node*,bool>	insert_ext(Node** Root, Data data){
+					// std::cout << "nb of times: " << data << std::endl;
+					Node*	y = NULL;
+					Node*	x = *Root;
+					int ht = 0;
+					while (x != NULL){
+						y = x;
+						if (comp(data.first, x->data.first))
+							x = x->left;
+						else if (data.first == x->data.first)
+							return ft::make_pair(x, false);
+						else
+							x = x->right;
+						ht++;
+					}
+					Node* z = node_new(data, y);
+					if (y == NULL){
+						*Root = z;
+					}
+					else if (comp(z->data.first, y->data.first)){
+						y->left = z;
+					}
+					else{
+						y->right = z;
+					}
+					
+					if (y != NULL){
+						Node *m = y;
+						while (m != NULL) {
+							m->height = max(height(m->right), height(m->left)) + 1;
+							m->bf = BF(m);
+							if (rebalance(m))
+								m = m->parent;
+							m = m->parent;
+						}
+					}
+					return ft::make_pair(z, true);
+
+				}
+				pair<Node*,bool>	insert(Node** Root, Data data){
+					pair<Node*,bool>	bobo = insert_ext(Root,data);
+					return bobo;
+				}
+
+				//insert
+				ft::pair<iterator, bool> insert (const value_type& val){
+				pair<Node*,bool> toto = _AVL.insert(&_AVL.root, val);
+				if (toto.second != false){
+					_size++;
+					return ft::make_pair<iterator, bool>(iterator(&toto.first->data, _AVL.root, toto.first), true);
+				}
+				return ft::make_pair<iterator, bool>(iterator(&toto.first->data, _AVL.root, toto.first), false);}
